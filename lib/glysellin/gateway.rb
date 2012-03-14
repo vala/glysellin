d324f4afe77837ed29862c95291c059491f787f2
vala <gonjo@free.fr>
2012-03-14 10:47:54 +0100

    Typo in order model

--- lib/glysellin/gateway.rb
+++ lib/glysellin/gateway.rb
@@ -1,9 +1,9 @@
 if File.exists? 'glysellin/engine/routes'
   require 'glysellin/gateway/base'
-  require 'glysellin/gateway/paypal'
+  require 'glysellin/gateway/paypal_integral'
 else
   require File.expand_path('../gateway/base', __FILE__)
-  require File.expand_path('../gateway/paypal', __FILE__)
+  require File.expand_path('../gateway/paypal_integral', __FILE__)
 end
 
 module Glysellin  
