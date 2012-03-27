1bf0880f59507d28f9e977842e3b50ffe81cdcfe
vala <gonjo@free.fr>
2012-03-27 09:03:20 +0200

    functionnal order <-> address relation for rails admin

--- app/models/glysellin/address.rb
+++ app/models/glysellin/address.rb
@@ -1,7 +1,10 @@
 module Glysellin
   class Address < ActiveRecord::Base
     self.table_name = 'glysellin_addresses'
-    has_many :orders
+    
+    has_many :billed_orders, :class_name => 'Glysellin::Order', :foreign_key => 'billing_address_id'
+    has_many :shipped_orders, :class_name => 'Glysellin::Order', :foreign_key => 'shipping_address_id'
+    
     # Additional fields can be added through Glysellin.config.additional_address_fields in the custom initializer
     store :additional_fields, :accessors => Glysellin.additional_address_fields
   end
