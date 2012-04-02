b14139edcba4b848ad32b2f63f5b1c369be0cec9
vala <gonjo@free.fr>
2012-04-02 15:29:52 +0200

    Check payment method & gateaway

--- lib/glysellin/gateway.rb
+++ lib/glysellin/gateway.rb
@@ -2,10 +2,12 @@ if File.exists? 'glysellin/engine/routes'
   require 'glysellin/gateway/base'
   require 'glysellin/gateway/paypal_integral'
   require 'glysellin/gateway/mercanet'
+  require 'glysellin/gateway/check'
 else
   require File.expand_path('../gateway/base', __FILE__)
   require File.expand_path('../gateway/paypal_integral', __FILE__)
   require File.expand_path('../gateway/mercanet', __FILE__)
+  require File.expand_path('../gateway/check', __FILE__)
 end
 
 module Glysellin  
---
+++ lib/glysellin/gateway/check.rb
@@ -0,0 +1,34 @@
+module Glysellin
+  module Gateway
+    class Check < Glysellin::Gateway::Base
+      register 'check', self
+
+      mattr_accessor :checks_order
+      @@checks_order = "NOBODY"
+      mattr_accessor :checks_destination
+      @@checks_destination = "NOWHERE"
+      
+      mattr_accessor :check_payment_description
+      @@check_payment_description = lambda { |order| I18n.t('glysellin.labels.payment_methods.check.send_your_check_text', :order_ref => order.ref, :check_order => @@checks_order, :check_destination => @@checks_destination) }
+
+      attr_accessor :errors, :order
+      
+      def initialize order
+        @order = order
+        @errors = []
+      end
+
+      def render_request_button
+         { :text => @@check_payment_description.call(@order) }
+      end
+
+      def process_payment! post_data
+        true
+      end
+
+      def response
+        { :nothing => true }
+      end
+    end
+  end
+end
