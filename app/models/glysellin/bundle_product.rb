5d83b7757c66481f4f74841389ca488e9be9b7e5
vala <gonjo@free.fr>
2012-07-18 16:19:07 +0200

    authorize mass assignment of bundle and product and ensure quantity present for validation in BundleProduct join model

--- app/models/glysellin/bundle_product.rb
+++ app/models/glysellin/bundle_product.rb
@@ -2,9 +2,12 @@ module Glysellin
   class BundleProduct < ActiveRecord::Base
     self.table_name = "glysellin_bundle_products"
 
-    attr_accessible :bundle_id, :product_id, :quantity
+    attr_accessible :bundle_id, :product_id, :quantity, :bundle, :product
     # Middle man association
     belongs_to :bundle
     belongs_to :product
+    
+    # Don't validate presence of product to be able create first bundle_product before one of these
+    validates_presence_of :quantity
   end
 end
