c613f78fbf78334451ae64becdd414a3327e56f2
vala <gonjo@free.fr>
2012-10-19 19:00:30 +0200

    add mail when an order with check payment method is passed

--- app/controllers/glysellin/orders_controller.rb
+++ app/controllers/glysellin/orders_controller.rb
@@ -2,11 +2,11 @@ module Glysellin
   class OrdersController < MainController
     protect_from_forgery :except => :gateway_response
     before_filter :get_customer!
-    
+
     def index
       @orders = current_user.orders
     end
-    
+
     def show
       user_order = Order.where('id = ? AND customer_id = ?', params[:id], @active_customer.id)
       if user_order.length > 0
@@ -16,7 +16,7 @@ module Glysellin
         redirect_to :action => 'index'
       end
     end
-    
+
     def process_order
       @order = Order.from_sub_forms(params[:order])
       # render :text => params.inspect + '<br/><br/>' + @order.items.inspect.gsub(/[<>]/, {'<' => '&lt;', '>' => '&gt;'})
@@ -24,52 +24,56 @@ module Glysellin
         next_step = @order.next_step
         if @order.next_step == Order::ORDER_STEP_PAYMENT
           OrderCustomerMailer.send_order_created_email(@order).deliver
+
+          if @order.payment_method.slug == 'check'
+            OrderAdminMailer.send_new_check_order_email(g.order).deliver
+          end
         end
         redirect_to :action => @order.next_step.to_s, :id => @order.ref
       else
         redirect_to :back
       end
     end
-    
+
     def
-    
+
     def cart
     end
-        
-    def validate_cart  
+
+    def validate_cart
     end
-    
+
     def fill_addresses
     end
-    
+
     def validate_addresses
     end
-  
+
     def recap
     end
-    
+
     def payment
       @order = Order.find_by_ref(params[:id])
     end
-    
+
     def offline_payment
     end
-  
+
     def gateway_response
       g = PaymentMethod.gateway(params[:goid] ? {:order_id => params[:goid]} : {:raw_post => request.raw_post, :gateway => params[:gateway]})
-      
+
       if g.process_payment! request.raw_post
         OrderCustomerMailer.send_order_paid_email(g.order).deliver
         OrderAdminMailer.send_order_paid_email(g.order).deliver
       end
-      
+
       if g.errors.length > 1
-        g.errors.each {|msg| logger.error "[ Glysellin ] Gateway Error : #{msg}"}  
+        g.errors.each {|msg| logger.error "[ Glysellin ] Gateway Error : #{msg}"}
       end
-      
+
       render g.response
     end
-  
+
     def payment_response
       if params[:type]
         @order = nil
--- app/mailers/glysellin/order_admin_mailer.rb
+++ app/mailers/glysellin/order_admin_mailer.rb
@@ -5,4 +5,9 @@ class Glysellin::OrderAdminMailer < ActionMailer::Base
     @order = order
     mail to: Glysellin.admin_email
   end
+
+  def send_new_check_order_email order
+    @order = order
+    mail to: Glysellin.admin_email
+  end
 end
