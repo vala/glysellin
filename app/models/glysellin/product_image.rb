6f30447fa1978d112596844acaee7af9394aa39d
vala <gonjo@free.fr>
2013-02-01 13:04:35 +0100

    mini code rewriting

--- app/models/glysellin/product_image.rb
+++ app/models/glysellin/product_image.rb
@@ -4,10 +4,8 @@ module Glysellin
 
     attr_accessible :image, :imageable_id, :imageable_type, :name
 
-
     belongs_to :imageable, :polymorphic => true
 
-    has_attached_file :image,
-      :styles => Glysellin.product_images_styles
+    has_attached_file :image, :styles => Glysellin.product_images_styles
   end
 end
